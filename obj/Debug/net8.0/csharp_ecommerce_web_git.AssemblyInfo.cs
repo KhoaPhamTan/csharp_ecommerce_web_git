b3eb0a1b0d32b32f9f892e057921c9a5e0b1c1ad
KhoaPhamTan <ptkhoa@yahoo.com>
2024-10-15 17:01:40 +0700

    update branch1.0

--- obj/Debug/net8.0/csharp_ecommerce_web_git.AssemblyInfo.cs
+++ obj/Debug/net8.0/csharp_ecommerce_web_git.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("csharp_ecommerce_web_git")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+993f0bb3f77365061d3405764a8b73c769da90b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f13367407ab233d49f0ebb0d3ff625065559259d")]
 [assembly: System.Reflection.AssemblyProductAttribute("csharp_ecommerce_web_git")]
 [assembly: System.Reflection.AssemblyTitleAttribute("csharp_ecommerce_web_git")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
